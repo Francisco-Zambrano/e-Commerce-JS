48a4c3b88e1526460db5f897d8583ef6b5dcd76c
Francisco Zambrano <136658451+Francisco-Zambrano@users.noreply.github.com>
2023-09-25 21:28:12 -0300

    mejorando maquetado

--- main.js
+++ main.js
@@ -1,57 +1,32 @@
 const contenedorProductos = document.querySelector("#contenedor-productos");
 
 
+
+
 fetch("./productos.json")
     .then(response => response.json())
     .then(data => {
-        data.forEach(producto => {
 
-            const div = document.createElement("div");
-                div.classList.add("producto");
-                div.innerHTML = `
-                
-                    <div class="card" style="width: 20rem;" id="producto">
-                        <img src="${producto.imagen}" class="card-img-top" id="imagen">
-                        <div class="card-body">
-                            <h5 class="card-title" id="titulo">${producto.titulo}</h5>
-                            <p class="card-text" id="descripcion">${producto.descripcion}</p>
-                            <h5 class="producto-precio">$${producto.precio}</h5>
-                            <a class="btn btn-outline-success" id="${producto.id}">Agregar</a>
-                        </div>
-                    </div>
-                    
-               `
-                  
-            contenedorProductos.append(div);        
-        })
+        subirProductos(data);
+       
     })
 
-
-
-
-
-
-
-// function subirProductos() {
-//     productos.forEach(producto => {
-//         const div = document.createElement("div");
-//         div.classList.add("producto");
-//         div.innerHTML = `
-        
-//             <div class="card" style="width: 20rem;" id="producto">
-//                 <img src="${producto.imagen}" class="card-img-top" id="imagen">
-//                 <div class="card-body">
-//                     <h5 class="card-title" id="titulo">${producto.titulo}</h5>
-//                     <p class="card-text" id="descripcion">${producto.descripcion}</p>
-//                     <h5 class="producto-precio">$${producto.precio}</h5>
-//                     <a class="btn btn-outline-success" id="${producto.id}">Agregar</a>
-//                 </div>
-//             </div>
+    function subirProductos(productos) {
+    productos.forEach(producto => {
+        const div = document.createElement("div");
+        div.classList.add("producto");
+        div.innerHTML = ` 
             
-//        `
+            <div class="card" id="producto">
+                <img id="imagen" src="${producto.imagen}">
+                <h3 id="titulo">${producto.titulo}</h3>
+                <p id="descripcion">${producto.descripcion}</p>
+                <h3 class="producto-precio">$${producto.precio}</h3>
+                <button class="producto-agregar" id="${producto.id}">Agregar</button>
+            </div>   
+       `
           
-//     contenedorProductos.append(div);
-//     });
-// }
-
-// subirProductos();
+    contenedorProductos.append(div);
+    });
+}
+   
